1bed278715c2037eea3ad3a1bf3784f1698cda6a
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-03-12 00:35:52 +0900

    후위표현식변한 알고리즘 완료 리팩토링 전

--- src/main/java/me/anpan/calculator/Calculator.java
+++ src/main/java/me/anpan/calculator/Calculator.java
@@ -1,23 +1,67 @@
 package me.anpan.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Calculator {
 
 
     public int calculate(String input) {
-
-
-
         return 0;
-
     }
 
     public String convertToPostfix(String input) {
-        String postFix = "";
-        Stack<String> operator = new Stack<>();
+        List<Character> result = new ArrayList<>();
+        Stack<Character> operator = new Stack<>();
+        char[] chars = input.toCharArray();
+        for(char arg : chars ) {
 
+            if('+' == arg || '-' == arg || '*' == arg || '/' == arg ) {
+                if(operator.empty()) {
+                    operator.push(arg);
+                    continue;
+                }
+                if(getOperatorPrioty(operator.peek()) >= getOperatorPrioty(arg) && getOperatorPrioty(operator.peek())>0 ) {
+                    Character popOperator = operator.pop();
+                    result.add(popOperator);
+                    operator.push(arg);
+                }else {
+                    operator.push(arg);
+                }
+            } else if('(' == arg) {
+                operator.push(arg);
+            } else if(')' == arg) {
+                while('('!= operator.peek()){
+                    result.add(operator.pop());
+                }
+                operator.pop();
+            } else {
+                result.add(arg);
+            }
+        }
+        if(!operator.empty()) {
+            while (!operator.empty()){
+                result.add(operator.pop());
+            }
+        }
+        StringBuffer postfix = new StringBuffer();
+        for (Character character : result) {
+            postfix.append(character.toString());
+        }
+        return postfix.toString();
+    }
 
-        return null;
+    public int getOperatorPrioty(Character operator) {
+        if('(' == operator || ')'==operator ) {
+            return 0;
+        }
+        if('+' == operator || '-'==operator ) {
+            return 1;
+        }
+        if('*' == operator || '/'==operator ) {
+            return 2;
+        }
+        return 99;
     }
 }
--- src/test/java/me/anpan/calculator/CalculatorApplicationTests.java
+++ src/test/java/me/anpan/calculator/CalculatorApplicationTests.java
@@ -24,12 +24,19 @@ public class CalculatorApplicationTests {
         String input ="2+2";
         assertEquals(6,calculator.calculate(input));
     }
+    @Test
+    public void 오퍼레이터의_우선순위를_확인한다() {
+        Character input = '/';
+        assertEquals(2,calculator.getOperatorPrioty(input));
+
+    }
 
     @Test
     public void 중위표현을_후위표현으로_변환() {
-        String input = "(2+5)*3*(2+1)";
-
-        assertEquals("25+3*21+*",calculator.convertToPostfix(input));
+        String input = "A*(B+C)";
+        String input2 = "(150+60/2)*2+(78-20+60)+1";
+        assertEquals("ABC+*",calculator.convertToPostfix(input));
+        assertEquals("150602/+2*7820-60++1+",calculator.convertToPostfix(input2));
     }
 
 }
