ab2595c2b65fb074ae7ee62318445eb91e82cfe5
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-03-21 01:23:39 +0900

    오타수정

--- src/main/java/me/anpan/calculator/service/Operator.java
+++ src/main/java/me/anpan/calculator/service/Operator.java
@@ -2,23 +2,23 @@ package me.anpan.calculator.service;
 
 public class Operator {
     private String symbol;
-    private int prioty;
+    private int priority;
 
     public Operator(String symbol) {
         this.symbol = symbol;
-        this.prioty = OperatorType.getPrioty(symbol);
+        this.priority = OperatorType.getPrioty(symbol);
     }
 
     public String getSymbol() {
         return symbol;
     }
 
-    public int getPrioty() {
-        return prioty;
+    public int getPriority() {
+        return priority;
     }
 
     public boolean isHigherPriority(String symbol) {
-        return prioty >= OperatorType.getPrioty(symbol);
+        return priority >= OperatorType.getPrioty(symbol);
     }
 
     public double calculate(double value1, double value2) {
